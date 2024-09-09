1bb23fbc6dcf9abe5f1e7b2dca72cad630d6c818
Haem <60575268+kutumbarayadu@users.noreply.github.com>
2024-09-09 19:02:42 +0530

    addpoup validation

--- src/app/pages/addpopup/addpopup.component.ts
+++ src/app/pages/addpopup/addpopup.component.ts
@@ -6,10 +6,17 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ActivatedRoute, Router } from '@angular/router';
-import Swal from 'sweetalert2';
+
 import { CustomerService } from '../../services/customersservice';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import Swal from 'sweetalert2';
+
+enum SwalMessageTypes {
+  Error = 'error',
+  Warning = 'warning',
+  Success = 'success',
+}
 @Component({
   selector: 'app-addpopup',
   standalone: true,
@@ -22,11 +29,13 @@ import { MatIconModule } from '@angular/material/icon';
     HttpClientModule,
     MatIconModule
   ],
+
   providers: [CustomerService], // Add this line
   templateUrl: './addpopup.component.html',
   styleUrls: ['./addpopup.component.scss'],
 })
 export class AddpopupComponent {
+
 navigateToEditCustomer(arg0: any) {
 throw new Error('Method not implemented.');
 }
@@ -60,7 +69,56 @@ throw new Error('Method not implemented.');
   //this.router.navigate(['/customer'])
 
  }
+ private showMessage(message: string, type: SwalMessageTypes) {
+  Swal.fire('', message, type);
+}
+nameValidator(control: any) {
+  const namePattern = /^[A-Za-z\s-]+$/;
+  if (!control.value || namePattern.test(control.value.trim())) {
+    return null;
+  }
+  return { invalidName: true };
+}
   onsubmit() {
+    if (!this.customerForm.valid) {
+      this.showMessage('Please correct the form.', SwalMessageTypes.Warning);
+      return;
+    }
+    const street = this.customerForm.get('street')?.value;
+    const streetValidation = this.nameValidator({ value: street });
+
+    if (streetValidation?.invalidName) {
+      Swal.fire('', 'Enter a valid street.', 'warning');
+      return;
+    }
+    const city = this.customerForm.get('city')?.value;
+    const cityValidation = this.nameValidator({ value: city });
+
+    if (cityValidation?.invalidName) {
+      Swal.fire('', 'Enter a valid  city.', 'warning');
+      return;
+    }
+    const country = this.customerForm.get('country')?.value;
+    const countryValidation = this.nameValidator({ value: country });
+
+    if (countryValidation?.invalidName) {
+      Swal.fire('', 'Enter a valid  country.', 'warning');
+      return;
+    }
+
+
+    const pincode = this.customerForm.get('pincode')?.value;
+    if (!/^[0-9]{6}$/.test(pincode)) {
+      Swal.fire('', 'Enter a valid pincode', 'warning');
+      return;
+    }
+    const state = this.customerForm.get('state')?.value;
+    const stateValidation = this.nameValidator({ value: state });
+
+    if (stateValidation?.invalidName) {
+      Swal.fire('', 'Enter a valid  state.', 'warning');
+      return;
+    }
     if (this.customerForm.valid) {
       const addressData = this.customerForm.value;
      addressData.customerId=this.data
@@ -90,6 +148,7 @@ throw new Error('Method not implemented.');
           // Optionally, navigate to another page
           this.router.navigateByUrl('/customer');
         },
+
         (error) => {
           console.error('Error adding customer:', error);
 
@@ -100,7 +159,9 @@ throw new Error('Method not implemented.');
             text: 'There was an issue adding the customer.',
             confirmButtonText: 'Try Again',
           });
+
         }
+
       );
     }
   }
